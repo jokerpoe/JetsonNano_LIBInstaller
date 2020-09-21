e52050c6a89c4807977a4fd8ce922b90714e6b1d
jokerpoe <61310578+jokerpoe@users.noreply.github.com>
2020-09-21 08:55:40 +0700

    Update Var7.sh

--- modules/Var7.sh
+++ modules/Var7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo '[VAR8] INSTALL WX' 
-sudo apt install make gcc libgtk-3-dev libwebkitgtk-dev libwebkitgtk-3.0-dev libgstreamer-gl1.0-0 freeglut3 freeglut3-dev python-gst-1.0 python3-gst-1.0 libglib2.0-dev ubuntu-restricted-extras libgstreamer-plugins-base1.0-dev libssl-dev
+sudo apt -y install make gcc libgtk-3-dev libwebkitgtk-dev libwebkitgtk-3.0-dev libgstreamer-gl1.0-0 freeglut3 freeglut3-dev python-gst-1.0 python3-gst-1.0 libglib2.0-dev ubuntu-restricted-extras libgstreamer-plugins-base1.0-dev libssl-dev
 sudo pip3 install wxpython
