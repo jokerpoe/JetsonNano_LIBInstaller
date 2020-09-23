c423f00da8b32f2fc52126a473e7e251ee1eebd9
jokerpoe <61310578+jokerpoe@users.noreply.github.com>
2020-09-23 15:05:44 +0700

    Update Var2.sh

--- modules/Var2.sh
+++ modules/Var2.sh
@@ -2,7 +2,11 @@
 
 echo '[7] Install DLIB and face_regconition'
 sudo apt-get install -y python3-matplotlib
-sudo pip3 install scikit-learn
+pip3 install --upgrade launchpadlib
+pip3 install --upgrade setuptools
+sudo pip3 install -U setuptools
+sudo apt-get install -y libpcap-dev libpq-dev
+sudo pip3 install cython
 sudo pip3 install pillow imutils scikit-image
 sudo pip3 install dlib -v
 sudo pip3 install face-recognition
