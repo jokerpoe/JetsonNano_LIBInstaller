10de234a9aaff9b83b4f79c5abaf2b0f96a0c11b
jokerpoe <61310578+jokerpoe@users.noreply.github.com>
2020-09-24 13:46:05 +0700

    Update Var3.sh

--- modules/Var3.sh
+++ modules/Var3.sh
@@ -10,13 +10,17 @@ sudo apt-get install -y libv4l-dev libxvidcore-dev libx264-dev
 sudo apt-get install -y libgtk-3-dev
 sudo apt-get install -y libatlas-base-dev gfortran
 sudo apt-get install -y python3-dev
-
-git clone https://github.com/opencv/opencv_contrib
+sudo pip3 install --upgrade numpy
 git clone https://github.com/opencv/opencv
+
+git clone https://github.com/opencv/opencv_contrib /home/$USER/opencv_contrib/
+
 cd opencv
 mkdir build
 cd build
 
+echo $PATH
+echo "PATH UPPER"
 sudo cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D CMAKE_INSTALL_PREFIX=/usr/local \
 -D INSTALL_PYTHON_EXAMPLES=ON \
@@ -27,15 +31,17 @@ sudo cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D OPENCV_DNN_CUDA=ON \
 -D ENABLE_FAST_MATH=1 \
 -D CUDA_FAST_MATH=1 \
--D CUDA_ARCH_BIN=5.3 \
+-D CUDA_ARCH_BIN=5.3,6.0,6.1,7.0,7.5 \
+-D CUDA_ARCH_PTX=7.5
 -D WITH_CUBLAS=1 \
--D OPENCV_EXTRA_MODULES_PATH=~/install/opencv_contrib/modules \
+-D OPENCV_EXTRA_MODULES_PATH=/home/$USER/opencv_contrib/modules \
 -D HAVE_opencv_python3=ON \
--D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda \
 -D WITH_LIBV4L=ON \
--D WITH_V4L=ON -D WITH_GSTREAMER=ON -D BUILD_TEST=OFF -D BUILD_PERF_TEST=OFF -D CUDNN_INCLUDE_DIR=/usr/lib/aarch64-linux-gnu/libcudnn.so -D BUILD_EXAMPLES=ON ..
+-D WITH_GTK=ON \
+-D PYTHON_EXECUTABLE=~/.virtualenvs/opencv_cuda/bin/python \
+-D BUILD_EXAMPLES=ON ..
 
-sudo make -j4
-sudo make install
-cd ..
-cd ..
+#sudo make -j4
+#sudo make install
+#cd ..
+#cd ..
