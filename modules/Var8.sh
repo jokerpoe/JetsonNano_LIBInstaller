2365398157cb47d7965f2bf161b1847807e55f91
jokerpoe <61310578+jokerpoe@users.noreply.github.com>
2020-10-23 08:58:45 +0700

    Update Var8.sh

--- modules/Var8.sh
+++ modules/Var8.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo '[VAR9] INSTALL NOMACHINE' 
-wget --spider https://download.nomachine.com/download/6.11/Arm/nomachine_6.11.2_1_arm64.deb
-sudo dpkg -i nomachine_6.11.2_1_arm64.deb
+wget https://download.nomachine.com/download/6.12/Arm/nomachine_6.12.3_5_arm64.deb
+sudo dpkg -i nomachine_6.12.3_5_arm64.deb
 
 yes | cp -rf org.gnome.Vino.gschema.xml /usr/share/glib-2.0/schemas/org.gnome.Vino.gschema.xml
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas
